01d347fa4ee31ce78972bea5a6dabfe63b7801dd
effus <effusps@gmail.com>
2019-12-09 19:14:50 +0300

    initial

---
+++ htdocs/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gemotest.Games</title>
+</head>
+<body>
+    <h1>Gemotest.Games</h1>
+</body>
+</html>
