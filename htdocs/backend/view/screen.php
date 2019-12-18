4577d787b4e2b07a65962733e4670a08f58cbaf9
effus <effusps@gmail.com>
2019-12-18 22:55:50 +0300

    qr

--- htdocs/backend/view/screen.php
+++ htdocs/backend/view/screen.php
@@ -25,6 +25,15 @@
         <h2><span class="badge badge-dark bg-gradient-secondary big-clock">%state%</span></h2>
     </div>
 </div>
+<div class="help mt-3">
+    <div class="col">
+        <p>Добро пожаловать!</p>
+        <p>Чтобы присоединиться, зайдите на своих мобильных девайсах по адресу: </p>
+        <code class="mb-2" style="font-size:24px;">http://gemotest.effus.beget.tech</code>
+        <p>Короткий URL: <code>http://bit.do/gemotest</code></p>
+        <p><img src="/frontend/imgs/qr.png" height="250px;" /></p>
+    </div>
+</div>
 <div class="row mt-5" id="answering">
     <div class="col">
         <h3>
